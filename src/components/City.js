00c0d9aa4fd65bdefe4876de3bf4d8f4ecafd0bf
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-05-13 16:32:43 +0530

    working for me

--- src/components/City.js
+++ src/components/City.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Town from "./Town";
 
 const City = ({ cities }) => {
+    const [cityname, setCityname] = useState("");
+
+    const showTown = (input) => {
+        if (input === cityname) {
+            setCityname("");
+        } else {
+            setCityname(input);
+        }
+    };
     return (
         <div>
             {cities.map((city, i) => {
                 return (
                     <div key={city.name}>
-                        <h1 id={`city${i + 1}`}>{city.name}</h1>
-                        <Town key={city.name} towns={city.towns} />
+                        <h1
+                            id={`city${i + 1}`}
+                            onClick={(e) => {
+                                showTown(e.target.innerText);
+                            }}
+                        >
+                            {city.name}
+                        </h1>
+                        <Town key={city.name} city={cityname} cityName={city.name} towns={city.towns} />
                     </div>
                 );
             })}
--- src/components/Town.js
+++ src/components/Town.js
@@ -1,15 +1,18 @@
 import React from "react";
 
-const Town = ({ towns }) => {
+const Town = (props) => {
+    const { city, cityName, towns } = props;
     return (
         <div>
-            {towns.map((town, i) => {
-                return (
-                    <div key={town.name}>
-                        <p id={`town${i + 1}`}>{town.name}</p>
-                    </div>
-                );
-            })}
+            {city === cityName
+                ? towns.map((town, i) => {
+                      return (
+                          <div key={town.name}>
+                              <p id={`town${i + 1}`}>{town.name}</p>
+                          </div>
+                      );
+                  })
+                : ""}
         </div>
     );
 };
